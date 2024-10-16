32171fd4cc8bb7c3261f52fde6d10a70ae9969d7
David Goitia <david@goitia.es>
2024-10-17 01:08:24 +0200

    Create gradle-publish.yml

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -105,6 +105,9 @@ publishing {
 }
 
 signing {
+    val signingKey: String? by project
+    val signingPassword: String? by project
+    useInMemoryPgpKeys(signingKey, signingPassword)
     sign(publishing.publications["mavenJava"])
 }
 
